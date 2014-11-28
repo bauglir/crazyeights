d4d46fee2324f83e0bf229d2fdb01a1877c261a7
Rob van den Hout <robvdhout@gmail.com>
2014-11-28 23:26:10 +0100

    Added game logic

---
+++ app/game_logic/game_logic.js
@@ -0,0 +1,27 @@
+/**
+ * @author Rob van den Hout
+ * @version 1.0.0
+ */
+
+(function game_logic(){
+
+	"use strict";
+
+	var myModule = angular.module('app', []);
+
+	myModule.factory('game_logic', function(){
+
+		var cards = {
+			1:
+		};
+
+		var players = {};
+
+		var ServiceInstance = {};
+
+
+
+		return ServiceInstance;
+	});
+
+})();
