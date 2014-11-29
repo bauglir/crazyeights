473f99964c9912113483757359e8b73c9175c7db
Danko Adamczyk <danko@studyportals.eu>
2014-11-29 03:44:03 +0100

    New view for the play-table.

---
+++ app/table/table.js
@@ -0,0 +1,16 @@
+/**
+ * Created by danko on 28-11-2014.
+ */
+
+(function () {
+	"use strict";
+	angular.module('app').controller('Table', Table);
+
+	Table.$inject = [];
+
+	function Table() {
+
+		var table = this;
+		table.players = 4;
+	}
+})();
