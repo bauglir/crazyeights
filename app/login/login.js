f08e5e111932447ac1caa97164f98acc88bb425a
Danko Adamczyk <danko@studyportals.eu>
2014-11-29 01:58:52 +0100

    Added some demo code to change an svg card based on an select field.

--- app/login/login.js
+++ app/login/login.js
@@ -10,12 +10,35 @@
 
 		var login = this;
 
+		login.cards = {
+			clubs_2c: {
+				name: 'Club 2',
+				path: 'Clubs/2c.svg'
+			},
+			clubs_3c: {
+				name: 'Club 3',
+				path: 'Clubs/3c.svg'
+			},
+			clubs_4c: {
+				name: 'Club 4',
+				path: 'Clubs/4c.svg'
+			},
+			clubs_5c: {
+				name: 'Club 5',
+				path: 'Clubs/5c.svg'
+			}
+		};
+
+		$scope.setCard = function(){
+
+			$scope.currentCard = 'svg%20playing%20cards/' + $scope.card;
+		}
+
 		$scope.submit = function () {
 
 			if ($scope.text) {
 
 				$scope.username = this.text;
-				login.username = this.text;
 				$scope.text = '';
 			}
 		};
