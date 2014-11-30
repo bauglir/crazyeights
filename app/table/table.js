f87c62069f789c915bba36f46d29adfd4d878272
Joris Kraak <me@joriskraak.nl>
2014-11-30 22:12:20 +0100

    Refactor table controller into a directive
    This is then used to show the current state of the table on the host
    screen.
    
    A note with respect to the table template. The slice on the cards is
    there to make sure that `reverse` is called is a copy of the original,
    since it modifies in-place.

--- app/table/table.js
+++ app/table/table.js
@@ -4,44 +4,26 @@
 
 (function () {
 	"use strict";
-	angular.module('app').controller('Table', Table);
+	angular.module('app').directive('table', function tableDirective() {
+    return {
+      controller: function TableController() {
+        var table = this;
 
-	Table.$inject = [];
+        table.getSvg = function(card){
+          var path = card.suit.charAt(0).toUpperCase() + card.suit.slice(1);
+          var file = card.rank + card.suit.charAt(0).toUpperCase() + '.svg';
 
-	function Table() {
+          return 'svg%20playing%20cards/' + path + '/' + file;
+        };
 
-		var table = this;
-
-		table.players = 4;
-
-		// TODO: get the userlist from the game_logic.
-
-		table.players = [
-			{username: 'Danko'},
-			{username: 'Inigo'},
-			{username: 'Wema'},
-			{username: 'Rob'},
-			{username: 'Joris'},
-			{username: 'David'}
-		];
-
-		table.cards = [
-			{id: 1,  suit: 'clubs', rank: 'A'},
-			{id: 2,  suit: 'clubs', rank: '2'},
-			{id: 3,  suit: 'clubs', rank: '3'},
-			{id: 4,  suit: 'clubs', rank: '4'},
-			{id: 5,  suit: 'clubs', rank: '5'}
-		];
-
-		table.getSvg = function(Card){
-
-			console.log(Card, this);
-
-			var $path = Card.suit.charAt(0).toUpperCase() + Card.suit.slice(1);
-			var $file = Card.rank + Card.suit.charAt(0).toUpperCase() + '.svg';
-
-			return 'svg%20playing%20cards/' + $path + '/' + $file;
-		};
-		
-	}
+        return table;
+      },
+      controllerAs: 'table',
+      restrict: 'E',
+      scope: {
+        cards: '='
+      },
+      templateUrl: 'app/table/table.html'
+    };
+  });
 })();
